0a90d614c196d4e7960e45430ab6eaa81e1d59df
Jonas83Carvalho <jonas.carvalho@gmail.com>
2021-11-09 21:51:00 -0300

    atualizações

--- airflow/dags/processando_precos.py
+++ airflow/dags/processando_precos.py
@@ -16,7 +16,7 @@ s3client = boto3.client("s3", aws_access_key_id=aws_access_key_id,
 
 # Usando a novíssima Taskflow API
 default_args = {
-    'owner': 'Neylson Crepalde',
+    'owner': 'Jonas Carvalho',
     "depends_on_past": False,
     "start_date": days_ago(2),
     "email": ["airflow@airflow.com"],
@@ -24,20 +24,20 @@ default_args = {
     "email_on_retry": False
 }
 
-@dag(default_args=default_args, schedule_interval=None, catchup=False, tags=["emr", "aws", "enem"], description="Pipeline para processamento de dados do ENEM 2019")
+@dag(default_args=default_args, schedule_interval=None, catchup=False, tags=["emr", "aws", "precos"], description="Pipeline para processamento de preços de alimentos no mundo.")
 def pipeline_enem():
     """
-    Pipeline para processamento de dados do ENEM 2019.
+    Pipeline para processamento de de preços de alimentos
     """
 
     @task
     def emr_process_enem_data():
         cluster_id = client.run_job_flow(
-            Name='EMR-Ney-IGTI',
+            Name='EMR-Jon-IGTI',
             ServiceRole='EMR_DefaultRole',
             JobFlowRole='EMR_EC2_DefaultRole',
             VisibleToAllUsers=True,
-            LogUri='s3://datalake-ney-igti-edc-tf/emr-logs',
+            LogUri='s3://datalake-igti-projeto-edc/emr-logs',
             ReleaseLabel='emr-6.3.0',
             Instances={
                 'InstanceGroups': [
@@ -56,10 +56,10 @@ def pipeline_enem():
                         'InstanceCount': 1,
                     }
                 ],
-                'Ec2KeyName': 'ney-igti-teste',
+                'Ec2KeyName': 'igti-1',
                 'KeepJobFlowAliveWhenNoSteps': True,
                 'TerminationProtected': False,
-                'Ec2SubnetId': 'subnet-1df20360'
+                'Ec2SubnetId': 'subnet-3125ac5a'
             },
 
             Applications=[{'Name': 'Spark'}],
@@ -109,7 +109,7 @@ def pipeline_enem():
                             '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
                             '--master', 'yarn',
                             '--deploy-mode', 'cluster',
-                            's3://datalake-ney-igti-edc-tf/emr-code/pyspark/01_delta_spark_insert.py'
+                            's3://datalake-igti-projeto-edc/script/01_delta_spark_insert.py'
                         ]
                 }
             }],
@@ -135,42 +135,6 @@ def pipeline_enem():
         )
         return True
 
-    @task
-    def upsert_delta(cid: str, success_before: bool):
-        if success_before:
-            newstep = client.add_job_flow_steps(
-                JobFlowId=cid,
-                Steps=[{
-                    'Name': 'Upsert da tabela Delta',
-                    'ActionOnFailure': "TERMINATE_CLUSTER",
-                    'HadoopJarStep': {
-                        'Jar': 'command-runner.jar',
-                        'Args': ['spark-submit',
-                                '--packages', 'io.delta:delta-core_2.12:1.0.0', 
-                                '--conf', 'spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension', 
-                                '--conf', 'spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog', 
-                                '--master', 'yarn',
-                                '--deploy-mode', 'cluster',
-                                's3://datalake-ney-igti-edc-tf/emr-code/pyspark/02_delta_spark_upsert.py'
-                            ]
-                    }
-                }]
-            )
-            return newstep['StepIds'][0]
-
-    @task
-    def wait_upsert_delta(cid: str, stepId: str):
-        waiter = client.get_waiter('step_complete')
-
-        waiter.wait(
-            ClusterId=cid,
-            StepId=stepId,
-            WaiterConfig={
-                'Delay': 30,
-                'MaxAttempts': 120
-            }
-        )
-        return True
 
 
     @task
@@ -184,9 +148,7 @@ def pipeline_enem():
     # Encadeando a pipeline
     cluid = emr_process_enem_data()
     res_emr = wait_emr_step(cluid)
-    newstep = upsert_delta(cluid, res_emr)
-    res_ba = wait_upsert_delta(cluid, newstep)
-    res_ter = terminate_emr_cluster(res_ba, cluid)
+    res_ter = terminate_emr_cluster(res_emr, cluid)
 
 
 execucao = pipeline_enem()
