048c8276807c3f1abab0393d421cdb5c3c055f4d
Jon83Carvalho <jonas.carvalho@gmail.com>
2021-11-13 00:40:54 -0300

    jupyter hub in emr

--- airflow/dags/processando_precos.py
+++ airflow/dags/processando_precos.py
@@ -62,7 +62,7 @@ def pipeline_precos():
                 'Ec2SubnetId': 'subnet-3125ac5a'
             },
 
-            Applications=[{'Name': 'Spark'}],
+            Applications=[{'Name': 'Spark'},{'Name':'JupyterHub'}],
 
             Configurations=[{
                 "Classification": "spark-env",
